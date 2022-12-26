aefbe8180aaf2c0308a1130e5b051c349be3bfd3
Mikhail-Koptev <103873802+Mikhail-Koptev@users.noreply.github.com>
2022-12-27 01:42:28 +0700

    Add files via upload

---
+++ page3.js
@@ -0,0 +1,25 @@
+let page = 0;
+
+window.addEventListener("keydown", (e) => {
+    if (e.key == "Up" || e.key == "ArrowUp") moveDown();
+    if (e.key == "Down" || e.key == "ArrowDown") moveUp();
+});
+
+async function moveUp() {
+    if (page == 0) window.location.replace("page4.html");
+    page = page + 1;
+    window.location.replace("#page"+page.toString());
+    if (page == -1) window.location.replace("page2.html");
+}
+async function moveDown() {
+    if (page == 0) window.location.replace("page4.html");
+    page = page - 1;
+    window.location.replace("#page"+page.toString());
+    if (page == -1) window.location.replace("page2.html");
+}
+
+async function onClick() {
+    let imgs = document.getElementById("images");
+    if (getComputedStyle(imgs).visibility == "hidden") imgs.style.visibility = "visible";
+    else if (getComputedStyle(imgs).visibility == "visible") imgs.style.visibility = "hidden";
+}
